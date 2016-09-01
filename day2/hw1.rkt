a22f44f45eb601ae17b5d3380504df65a7ecd3cc
lynnandreastein <las@olin.edu>
2016-09-01 12:11:36 -0400

    Update hw1.rkt

--- day2/hw1.rkt
+++ day2/hw1.rkt
@@ -7,14 +7,32 @@
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
 
-;; 1.  WRITE FACTORIAL:  given n, returns n!
+;;     NOTE:  You need not worry about error checking in the programs below.
+
+;; 1.  WRITE SQUARE:  given n, returns n^2.  Hint:  use *
+(define (square x)
+  your-code-here)
+  
+(display (square 2)) (newline)  ;; -> 4
+
+;; 2.  WRITE is-right-triangle:  given three numbers, returns true iff the third
+;;     could be the hypotenuse of a right triangle with the specified three side lengths
+;;     Hint:  use = to compare numeric values
+
+(define (is-right-triangle)
+  your-code-here)
+  
+(display (is-right-triangle 3 4 5)) (newline)  ;; -> #t
+(display (is-right-triangle 4 5 6)) (newline)  ;; -> #f
+
+;; 3.  WRITE FACTORIAL:  given n, returns n!
 (define (fact n)
   your-code-here)
 
 (display (fact 1)) (newline) ;; -> 1
 (display (fact 2)) (newline) ;; -> 1
 
-;; 2.  WRITE FIBONACCI:  given n, returns the nth fibonacci number as shown below
+;; 4.  WRITE FIBONACCI:  given n, returns the nth fibonacci number as shown below
 (define (fib n)
    your-code-here)
 
@@ -25,14 +43,14 @@
 (display (fib 5)) (newline) ;; -> 5
 (display (fib 6)) (newline) ;; -> 8
 
-;; 3.  WRITE a procedure that takes a list of numbers and returns the sum of those numbers
+;; 5.  WRITE a procedure that takes a list of numbers and returns the sum of those numbers
 (define (sum lst)
   your-code-here)
 
 (display (sum '(1 2 3 4))) (newline) ;; -> 10
 (display (sum '(1 20 300))) (newline) ;; -> 321
 
-;; 4.  WRITE a procedure that takes a list of numbers and returns the largest one.
+;; 6.  WRITE a procedure that takes a list of numbers and returns the largest one.
 ;;     While there are solutions using scheme's built-in max, we were actually hoping you'd do something else...
 (define (my-max lst)
   your-code-here)
