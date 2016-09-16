75ae53364f385e1419600a64b35c74cd17f9009a
Oliver Steele <steele@osteele.com>
2016-09-15 22:13:44 -0400

    add day5/hw4

---
+++ day5/hw4.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+;;; Student Name: Frankly Olin [change to your name]
+;;;
+;;; Check one:
+;;; [ ] I completed this assignment without assistance or external resources.
+;;; [ ] I completed this assignment with assistance from ___
+;;;     and/or using these external resources: ___
